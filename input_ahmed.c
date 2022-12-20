1a6e2943700b1eee48029fabe474038ce058a445
Ahmed Youssef <a.goerany@gmail.com>
2022-12-20 16:31:05 +0200

    almost

--- input_ahmed.c
+++ input_ahmed.c
@@ -22,8 +22,11 @@ void lowercase(char * word)
         word[i] = tolower(word[i]);
     }
 }
+static int count = 0; // counts number of parameters assigned successfully
 
-void getParameters(char[] strline, int[] parameters);
+void getParameters(char strline[], long parameters[], int k);
+
+void getInt(char strline[], long parameters[], int index);
 
 int main()
 {
@@ -61,7 +64,7 @@ int main()
             break;
         }
     } while (1);
-    int height, width, highscores;
+    long height, width, highscores;
     char strline[500];
     if (counter == 3)
     {
@@ -72,24 +75,100 @@ int main()
     else
     {
         FILE * xml_file = fopen(path, "r");
-        int parameters[3]; // for each parameter (Height, width, highscore in that order)
+        long parameters[3]; // for each parameter (Height, width, highscore in that order)
+        int k = 0;
         while(strline != NULL)
         {
-            fscanf(xml_file, "%s%[^\n]%n", strline);
+            fscanf(xml_file, "%s", strline);
             // for each line
+            getParameters(strline, parameters, k);
+            if (count == 3)// all parameters are assigned a value
+            {
+                break;
+            }
         }
-
+        height = parameters[0];
+        width = parameters[1];
+        highscores = parameters[2];
     }        
-    printf("%s", strline);
+    printf("Height: %ld\nWidth: %ld\nHigh Scores: %ld\n", height, width, highscores);
     free(path);
 }
 
-void getParameters(char strline[], int parameters[])
+void getParameters(char strline[], long parameters[], int k)
 {
     lowercase(strline);
     // for each parameter
-    if (strstr(strline, "height") != NULL)
+    if (strstr(strline, "height") != NULL) // if the word "height" appears in the line, then we look for its value
     {
-        scanf("%*s %d%n", &parameters[0]); 
+        getInt(strline, parameters, 0); // index of height is zero
     } 
+    else if (strstr(strline, "width") != NULL) // if the word "width" appears in the line, then we look for its value
+    {
+        getInt(strline, parameters, 1); // index of width is one
+    }
+    else if (strstr(strline, "highscores") != NULL) // if the word "highscores" appears in the line, then we look for its value
+    {
+        getInt(strline, parameters, 2); // index of highscores is two
+    }
+    else
+    // if the words "height/width/high scores" aren't mentioned in the config we will use the order "Height -> Width -> HighScore"
+    {
+        getInt(strline, parameters, k);
+        k++;
+    }  
+}
+void getInt(char strline[], long parameters[], int index) // function extracts integer from string
+{
+    char * ptr = strline;
+    while(*ptr)
+    {
+        if (isdigit(*ptr) || (*ptr == '-' || *ptr == '+') && isdigit(*(ptr + 1)))
+        {
+            parameters[index] = strtol(ptr, &ptr, 10);
+            count++;
+        }
+        else
+        {
+            ptr++;
+        }
+    }
+    switch (index)
+    {
+        case 0:
+            if (parameters[0] <=3)
+            {
+                red();
+                printf("You entered an invalid height. (MUST be higher than 3)\n");
+                yellow();
+                printf("We are going to use our default configuration for height...\n");
+                reset_color();
+                parameters[0] = 7;
+            }
+            break;
+        case 1:
+            if (parameters[1] <=3)
+            {
+                red();
+                printf("You entered an invalid width. (MUST be higher than 3)\n");
+                yellow();
+                printf("We are going to use our default configuration for width...\n");
+                reset_color();
+                parameters[1] = 9;
+            }
+            break;
+        case 2:
+            if (parameters[2] <= 0)
+            {
+                red();
+                printf("You entered an invalid maximum high score. (MUST be higher than zero)\n");
+                yellow();
+                printf("We are going to use our default configuration for width...\n");
+                reset_color();
+                parameters[2] = 10;
+            }
+            break;
+        default:
+            break;            
+    }
 }
