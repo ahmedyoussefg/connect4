88d00872bced25ff53d9c10094451b52770eac7c
Ahmed Youssef <a.goerany@gmail.com>
2022-12-22 20:18:29 +0200

    structure doccumentation

--- types.h
+++ types.h
@@ -1,6 +1,8 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+/// @brief Player data type, has attributes: name (if he is winner), score, id(1 or 2) in case of vsPlayerMode/not in vscomputer mode
+///        , symbol (he plays with), highscore(maximum score of the player)
 typedef struct {
     char name[100];
     int score;
