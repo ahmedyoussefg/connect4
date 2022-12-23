5e7bb9447af4b9c518aea8b488a51f4f717947a4
Ahmed Youssef <a.goerany@gmail.com>
2022-12-23 23:39:58 +0200

    Added configurations type structure

--- types.h
+++ types.h
@@ -2,13 +2,21 @@
 #define TYPES_H
 
 /// @brief Player data type, has attributes: name (if he is winner), score, id(1 or 2) in case of vsPlayerMode/not in vscomputer mode
-///        , symbol (he plays with), highscore(maximum score of the player)
+///        , symbol (he plays with), highscore(maximum score of the player), color of the player.
 typedef struct {
     char name[100];
     int score;
     int id;
     char symbol;
     int highscore;
+    int color;
 } player;
+
+/// @brief  configuration of the game-board "Height, Width, Maximum Number of Displayed High Scores"
+typedef struct {
+    int height;
+    int width;
+    int highscores;
+} configuration;
 
 #endif
